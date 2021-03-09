8e5071c16ccd4f986b169832630cdb5f40730fa1
Yanis <megayanis@live.fr>
2021-03-09 14:47:06 +0100

    Initial Commit

---
+++ js/services.js
@@ -0,0 +1,101 @@
+/***********************************************************************************
+ * App Services. This contains the logic of the application organised in modules/objects. *
+ ***********************************************************************************/
+
+myApp.services = {
+
+  /////////////////
+  // Task Service //
+  /////////////////
+  tasks: {
+
+    // Creates a new task and attaches it to the pending task list.
+    create: function (data) {
+      // Task item template.
+      var taskItem = ons.createElement(
+        //'<ons-list-item tappable category="' + myApp.services.categories.parseId(data.category)+ '">' +
+        '<ons-list-item tappable category="' + data.category + '">' +
+        '<label class="left">' +
+        '<ons-checkbox></ons-checkbox>' +
+        '</label>' +
+        '<div class="center">' +
+        data.title +
+        '</div>' +
+        '<div class="right">' +
+        '<ons-icon style="color: grey; padding-left: 4px" icon="ion-ios-trash-outline, material:md-delete"></ons-icon>' +
+        '</div>' +
+        '</ons-list-item>'
+      );
+
+      // Store data within the element.
+      taskItem.data = data;
+
+      // Insert urgent tasks at the top and non urgent tasks at the bottom.
+      var pendingList = document.querySelector('#pending-list');
+      pendingList.insertBefore(taskItem, taskItem.data.urgent ? pendingList.firstChild : null);
+    },
+
+  },
+
+  ////////////////////////
+  // Initial Data Service //
+  ////////////////////////
+  fixtures: [
+    {
+      title: 'Download OnsenUI',
+      category: 'Programming',
+      description: 'Some description.',
+      highlight: false,
+      urgent: false
+    },
+    {
+      title: 'Install Monaca CLI',
+      category: 'Programming',
+      description: 'Some description.',
+      highlight: false,
+      urgent: false
+    },
+    {
+      title: 'Star Onsen UI repo on Github',
+      category: 'Super important',
+      description: 'Some description.',
+      highlight: false,
+      urgent: false
+    },
+    {
+      title: 'Register in the community forum',
+      category: 'Super important',
+      description: 'Some description.',
+      highlight: false,
+      urgent: false
+    },
+    {
+      title: 'Send donations to Fran and Andreas',
+      category: 'Super important',
+      description: 'Some description.',
+      highlight: false,
+      urgent: false
+    },
+    {
+      title: 'Profit',
+      category: '',
+      description: 'Some description.',
+      highlight: false,
+      urgent: false
+    },
+    {
+      title: 'Visit Japan',
+      category: 'Travels',
+      description: 'Some description.',
+      highlight: false,
+      urgent: false
+    },
+    {
+      title: 'Enjoy an Onsen with Onsen UI team',
+      category: 'Personal',
+      description: 'Some description.',
+      highlight: false,
+      urgent: false
+    }
+  ]
+};
